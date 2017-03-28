83d58ee8574983b323057de2a84812ec7c9f1f53
Covor Sorin <sorincovor@yahoo.com>
2017-03-28 09:36:17 +0300

    Update bonjour_process.py

--- Devoir 2/bonjour_process.py
+++ Devoir 2/bonjour_process.py
@@ -1,12 +1,5 @@
 """
-Le premier prgramme en Python
-* utilisation des arguments de la lignne de commande
-* les processus
-* le logger
-
-@author Dragos STOICA
-@version 0.6
-@date 17.feb.2014
+@author Sorin COVOR
 """
 
 import time
